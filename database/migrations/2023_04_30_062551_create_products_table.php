8d2fc5ba375c7e56943bce7429f80842a032e79d
shofique21 <mislam629@gmail.com>
2023-05-01 10:53:16 +0600

    product factory

--- database/migrations/2023_04_30_062551_create_products_table.php
+++ database/migrations/2023_04_30_062551_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('short_description')->nullable();
             $table->text('description')->nullable();
             $table->string('SKU')->nullable();
             $table->bigInteger('category_id')->unsigned();
